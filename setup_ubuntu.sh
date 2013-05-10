d9092300f53820521a50d0c82a4eeea823445a9e
crazygit <lianglin999@gmail.com>
2013-05-10 14:43:20 +0800

    fix color man pages

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -59,17 +59,16 @@ function config_software()
     # 给man pages添加颜色
     BASHRC_FILE="$HOME/.bashrc"
     cat >> $BASHRC_FILE <<'EOF'
-man() {
-    env \
-    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-    LESS_TERMCAP_md=$(printf "\e[1;31m") \
-    LESS_TERMCAP_me=$(printf "\e[0m") \
-    LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-    LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
-    man "$@"
-}
+
+#color man pages
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
 EOF
     # 让命令行支持vi模式
     echo "set -o vi" >> $BASHRC_FILE
