dd33d1eec4fffbcb0ff0b6bdd307eaa3fc868d12
crazygit <linliang@gmail.com>
2013-06-12 18:22:33 +0800

    add more function to setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,20 +3,20 @@
 function init_var()
 {
     GIT_USERNAME='crazygit'
-    GIT_EMAIL="linliang@gmail.com"
+    GIT_EMAIL="lianglin999@gmail.com"
 }
 
 
 # 因为要修改源等和安装软件，需要root权限，所以先获得ROOT权限
-function is_root()
-{
-    ROOT_UID=0
-
-    if [ $UID -ne $ROOT_UID ];then
-        echo "This script need ROOT right, Please run as root."
-        exit
-    fi
-}
+#function is_root()
+#{
+#    ROOT_UID=0
+#
+#    if [ $UID -ne $ROOT_UID ];then
+#        echo "This script need ROOT right, Please run as root."
+#        exit
+#    fi
+#}
 
 
 # 使用163的更新源，同时加上JDK的源
@@ -27,9 +27,9 @@ function update_apt_source()
         echo "No UBUNTU_CODENAME assigend"
         exit 1
     fi
-    JDK_URL="deb http://us.archive.ubuntu.com/ubuntu/ hardy multiverse"
+    JDK_URL="deb http://archive.canonical.com/ lucid partner"
     APT_FILE="/etc/apt/sources.list"
-    mv $APT_FILE $APT_FILE.bak
+    sudo mv $APT_FILE $APT_FILE.bak
 
     cat >> $APT_FILE << EOF
 deb http://mirrors.163.com/ubuntu/ $UBUNTU_CODENAME main restricted universe multiverse
@@ -41,18 +41,25 @@ $JDK_URL
 EOF
     # 添加nvidia显卡驱动地址
     sudo add-apt-repository ppa:ubuntu-x-swat/x-updates
-    apt-get update && apt-get -y upgrade
+    # 添加fcitx源
+    sudo add-apt-repository ppa:fcitx-team/nightly
+    sudo apt-get update && sudo apt-get -y upgrade
 }
 
 
 # 安装常用软件
 function install_software()
 {
-    apt-get install -y vim vim-gnome git-core chromium-browser ipython tree sun-java6-jdk \
-        flashplugin-installer gnome-shell python-pip virtualbox nvidia-current \
-        nvidia-current-modaliases nvidia-settings vlc stardict etckeeper
+    sudo apt-get install -y vim vim-gnome git-core chromium-browser ipython tree sun-java6-jdk \
+        flashplugin-installer python-pip virtualbox nvidia-current \
+        nvidia-current nvidia-settings vlc stardict etckeeper ctags curl
 
-    pip install markdown
+    sudo pip install markdown flake8
+
+    #安装 fcitx输入法
+    sudo apt-get install -y fcitx fcitx-config-gtk fcitx-sunpinyin fcitx-googlepinyin fcitx-module-cloud \
+         fcitx-table-all
+    im-switch -s fcitx -z default
 }
 
 
@@ -67,10 +74,7 @@ function config_software()
     # 配置vim
     ssh -o StrictHostKeyChecking=no git@github.com
     git clone git://github.com/crazygit/vimconf.git ~/.vim
-    ln -s ~/.vim/vimrc ~/.vimrc
-    cd ~/.vim
-    git submodule init
-    git submodule update
+    bash ~/.vim/install.sh
 
     # 给man pages添加颜色
     BASHRC_FILE="$HOME/.bashrc"
@@ -93,4 +97,34 @@ EOF
     # 增加记录HISTORY的条数
     sed -i 's/HISTSIZE=1000/HISTSIZE=10000/' $BASHRC_FILE
     sed -i 's/HISTFILESIZE=2000/HISTFILESIZE=20000/' $BASHRC_FILE
+}
+
+function download_githubs
+{
+    current_user=$USER
+    github_dir=/data/github
+    sudo mkdir -p $github_dir
+    sudo chown -R $current_user:$current_user /data
+    if [ ! -d $github_dir ];then
+        echo "Create <$github_dir> dir failed. use $HOME instead"
+        github_dir=$HOME/github
+        mkdir -p $github_dir
+    fi
+    cd $github_dir
+    git clone https://github.com/crazygit/crazygit.github.com.git
+    git clone https://github.com/crazygit/scriptcookies.git
+    cd $github_dir/crazygit.github.com && git remote set-url origin git@github.com:crazygit/crazygit.github.com.git
+    init_blog=$github_dir/crazygit.github.com/init_env.sh
+    test -f $init_blog && bash $init_blog
+    cd $github_dir/scriptcookies && git remote set-url origin git@github.com:crazygit/scriptcookies.git
+}
+
+
+function main()
+{
+   init_var
+   update_apt_source
+   install_software
+   config_software
+   download_githubs
 }
